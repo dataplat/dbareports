2a1e3f1746bd50fa70d03595f6a882186fcc24b1
Squaffle <Squaffle@hotmail.com>
2016-08-24 23:09:00 +0100

    Various minor help formatting/typo/case changes. Not sure purpose of Get-DbrConfig vs Get-Config.

--- functions/Add-DbrAgentJob.ps1
+++ functions/Add-DbrAgentJob.ps1
@@ -54,7 +54,7 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 .LINK
-https://dbatools.io/functions/
+https://dbareports.io/functions/Add-DbrAgentJob
 
 .EXAMPLE
 Add-DbrAgentJob -JobName 'Agent Job to gather information' -LogFileFolder 'H:\LogFiles' -Description 'This agent job will gather information and will log to folder H:\LogFiles' -Category 'dba collection jobs' -OwnerLoginName 'THEBEARD\Rob' -Command $GatheringScript -Subsystem TransactSql 
