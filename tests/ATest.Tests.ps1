0388f51a03bf519aad2c3840ce5180b2d659fd44
SQLDBAWithABeard <mrrobsewell@outlook.com>
2016-08-29 15:34:21 +0100

    Added appveyor file and tests

---
+++ tests/ATest.Tests.ps1
@@ -0,0 +1,7 @@
+﻿Describe "A First test!" {
+    Context "Test some code here" {
+        It "Should do some testing but doesnt yet" {
+        $true |should be true
+        }
+    }
+}
---
+++ tests/AnotherTest.Tests.ps1
@@ -0,0 +1,7 @@
+﻿Describe "A Second test!" {
+    Context "More Test some code here" {
+        It "Should do some more testing but doesnt yet" {
+        $true |should be true
+        }
+    }
+}
