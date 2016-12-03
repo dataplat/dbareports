907ffbdbe7236f7d49b146f4c416cbd8ed957df1
SQLDBAWithABeard <mrrobsewell@outlook.com>
2016-12-03 08:43:15 +0000

    Removed updatre column for databases table - so tvp will correctly insert data

--- setup/database/Tables/info.Databases.sql
+++ setup/database/Tables/info.Databases.sql
@@ -42,8 +42,7 @@ CREATE TABLE [info].[Databases]
 [LastRead] [datetime2] NULL,
 [LastWrite] [datetime2] NULL,
 [LastReboot] [datetime2] NULL,
-[LastDBCCDate] [datetime] NULL,
-[Update] [bit] NULL
+[LastDBCCDate] [datetime] NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [info].[Databases] ADD CONSTRAINT [PK_Databases] PRIMARY KEY CLUSTERED  ([DatabaseID]) ON [PRIMARY]
